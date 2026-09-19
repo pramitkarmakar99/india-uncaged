cdd04092ade993c16d18b3a4662bfca9feb049b2
pramitkarmakar99 <81361747+pramitkarmakar99@users.noreply.github.com>
2026-09-20 03:42:05 +0530

    Add enquiry tour relationships

--- app/Models/Enquiry.php
+++ app/Models/Enquiry.php
@@ -2,6 +2,8 @@
 namespace App\\Models;
 use Illuminate\\Database\\Eloquent\\Model;
 class Enquiry extends Model {
+ public function tour(){ return $this->belongsTo(\App\Models\Tour::class); }
+ public function tourDate(){ return $this->belongsTo(\App\Models\TourDate::class); }
  protected $guarded=[];
  protected $casts=['interests'=>'array'];
 }
