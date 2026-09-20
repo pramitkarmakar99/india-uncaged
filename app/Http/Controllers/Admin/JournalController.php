34c5947e55c6f4c33a9334b6240c6c7c719a4de7
pramitkarmakar99 <81361747+pramitkarmakar99@users.noreply.github.com>
2026-09-20 11:24:55 +0530

    Correct journal SEO image validation rule

--- app/Http/Controllers/Admin/JournalController.php
+++ app/Http/Controllers/Admin/JournalController.php
@@ -25,7 +25,7 @@ class JournalController extends Controller
             'excerpt'=>['nullable','string','max:2000'],'cover_image'=>['nullable','string','max:2048','regex:/^(https?:\/\/|\/storage\/)[^\s]+$/i'],'author'=>['nullable','string','max:255'],
             'seo_image'=>['nullable','string','max:2048','regex:/^(https?:\/\/|\/storage\/)[^\s]+$/i'],
             'content'=>['nullable','string'],'seo_title'=>['nullable','string','max:255'],'seo_description'=>['nullable','string'],
-            'seo_image'=>['nullable','string','max:2048'],'published'=>['nullable','boolean'],
+            'published'=>['nullable','boolean'],
         ]);
         $slug=Str::slug($data['slug'] ?: $data['title']);
         if(JournalArticle::where('slug',$slug)->whereKeyNot($article->id ?? 0)->exists()) $slug.='-'.Str::lower(Str::random(5));
